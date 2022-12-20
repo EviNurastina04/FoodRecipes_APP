e30bfb3642e6ef1c6004dd30868669c57086d648
Evi Nurastina <102676422+EviNurastina04@users.noreply.github.com>
2022-12-20 18:45:29 +0700

    Update FoodViewModelProviderFactory.kt

--- FoodViewModelProviderFactory.kt
+++ FoodViewModelProviderFactory.kt
@@ -1,9 +1,9 @@
-package com.example.foodapp.ui.viewModels
+package com.evinurastina.foodapp.ui.viewModels
 
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.foodapp.repository.FoodRepository
+import com.evinurastina.foodapp.repository.FoodRepository
 
 class FoodViewModelProviderFactory(val app:Application,val foodRepository:FoodRepository) : ViewModelProvider.Factory{
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
