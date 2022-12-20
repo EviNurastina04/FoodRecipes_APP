31b356e0a7f331b21326dff9b988642cf48341c2
Evi Nurastina <102676422+EviNurastina04@users.noreply.github.com>
2022-12-20 18:30:58 +0700

    Update and rename BuildConfig.java to BuildConfig.kt

--- BuildConfig.kt
+++ BuildConfig.kt
@@ -1,7 +1,7 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.example.foodapp;
+package com.evinurastina.foodapp;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
