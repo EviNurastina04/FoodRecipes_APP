4f53256ce91a2308933a0bdc206d7d3717daca6b
Evi Nurastina <102676422+EviNurastina04@users.noreply.github.com>
2022-12-20 19:04:26 +0700

    Update RetrofitInstance.kt

--- RetrofitInstance.kt
+++ RetrofitInstance.kt
@@ -1,6 +1,6 @@
-package com.example.foodapp.api
+package com.evinurastina.foodapp.api
 
-import com.example.foodapp.util.Constants.Companion.BASE_URL
+import com.evinurastina.foodapp.util.Constants.Companion.BASE_URL
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
